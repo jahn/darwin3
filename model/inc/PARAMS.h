bf2e3caa923c21c0bec8c5f62d0535394bf4aa92
Oliver Jahn <jahn@mit.edu>
2022-03-24 18:01:34 -0400

    Add sal package

--- model/inc/PARAMS.h
+++ model/inc/PARAMS.h
@@ -1086,6 +1086,7 @@ C-- Logical flags for selecting packages
       LOGICAL useMNC
       LOGICAL useRunClock
       LOGICAL useEMBED_FILES
+      LOGICAL useSAL
       LOGICAL useMYPACKAGE
       COMMON /PARM_PACKAGES/
      &        useGAD, useOBCS, useSHAP_FILT, useZONAL_FILT,
@@ -1099,7 +1100,7 @@ C-- Logical flags for selecting packages
      &        useStreamIce, useICEFRONT, useThSIce, useLand,
      &        useATM2d, useAIM, useAtm_Phys, useFizhi, useGridAlt,
      &        useDiagnostics, useREGRID, useLayers, useMNC,
-     &        useRunClock, useEMBED_FILES,
+     &        useRunClock, useEMBED_FILES, useSAL,
      &        useMYPACKAGE
 
 C---+----1----+----2----+----3----+----4----+----5----+----6----+----7-|--+----|

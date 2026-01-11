f163ad013e61b62790ee3a152111a8a6f22968b2
Oliver Jahn <jahn@mit.edu>
2026-01-11 09:16:37 -0500

    Add darwin_oned_examples testing to github actions

---
+++ tools/ci/runtr_oned.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Test report running script for Travis CI 
+# - called from .travis.yml script: section with environment variables 
+#   possibly set.
+#   If they are not set then defaults are configured here.
+# MITGCM_DECMD - command to run under Docker
+# MITGCM_EXP   - MITgcm test to run
+# MITGCM_TROPT - Test report options
+#
+
+if [ -z "${MITGCM_TROPT}" ]; then
+ export MITGCM_TROPT='-devel -of=../tools/build_options/linux_amd64_gfortran -match 14'
+fi
+if [ -z "${MITGCM_DECMD}" ]; then
+ export MITGCM_DECMD='docker exec -i testreport-ubuntu bash -c'
+fi
+if [ -z "${MITGCM_INPUT_DIR_PAT}" ]; then
+ export MITGCM_INPUT_DIR_PAT='/input.*'
+fi
+
+${MITGCM_DECMD} "cd /MITgcm/darwin_oned_examples; ./testreport -pass -short -t ${MITGCM_EXP} ${MITGCM_TROPT}"
